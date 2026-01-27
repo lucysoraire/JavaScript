f30c548ea01e0e805b9c02f5d9d39d60f06920b7
lucia soraire <luciasoraire@gmail.com>
2026-01-27 17:54:30 -0300

    Chat Bot

---
+++ Workshop/BuildAGreetingBot.js
@@ -0,0 +1,54 @@
+
+// Add a console.log() statement that outputs the string "Hi there!" to the console. Don't forget your quotes around the message!
+console.log("Hi there!");
+
+// Add another console.log statement to output the message "I am excited to talk to you." to the console.
+console.log("I am excited to talk to you.")
+
+// Use the let keyword to declare a variable called bot.
+let bot;
+
+// Assign the string "teacherBot" to the bot variable.
+bot = "teacherBot";
+
+// Declare and assign the string "the universe" to the botLocation variable on the same line using the let keyword.
+let botLocation = "the universe";
+
+// Add another console statement to the code that logs the message "Allow me to introduce myself.".
+console.log("Allow me to introduce myself.");
+
+// Assign this value to the botIntroduction variable.
+// Then, log the botIntroduction variable to the console.
+const botIntroduction = "My name is " + bot + ".";
+console.log(botIntroduction);
+
+// Assign this value to the botLocationSentence variable.
+//Then, log the value of botLocationSentence to the console.
+const botLocationSentence = "I live in " + botLocation + ".";
+console.log(botLocationSentence);
+
+// Using reassignment, assign the string "professorBot" to the bot variable.
+bot = "professorBot";
+
+// Assign the resulting string to the nicknameIntroduction variable.
+// Then, log the value of nicknameIntroduction to the console.
+const nicknameIntroduction = "My nickname is " + bot + ".";
+console.log(nicknameIntroduction);
+
+// Using reassignment, assign the string "awesomeTeacherBot" to the bot variable.
+bot = "awesomeTeacherBot";
+
+// Assign the result to the newNicknameGreeting variable.
+//Then, log the value of newNicknameGreeting to the console.
+const newNicknameGreeting = "I love my nickname but I wish people would call me " + bot + ".";
+console.log(newNicknameGreeting);
+
+// Start by creating a new variable called favoriteSubject and assign it the string "Computer Science".
+const favoriteSubject = "Computer Science";
+
+// Assign the result to the favoriteSubjectSentence variable.
+//Then, log the value of favoriteSubjectSentence to the console.
+const favoriteSubjectSentence = "My favorite subject is " + favoriteSubject + ".";
+
+// For the final step, you will log the bot's message of "Well, it was nice to talk to you. Have a nice day!" to the console.
+console.log(favoriteSubjectSentence);
