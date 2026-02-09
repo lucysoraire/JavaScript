b79862a3651f2d015c836e108bbad11dd26b2c21
lucia soraire <luciasoraire@gmail.com>
2026-02-09 18:04:46 -0300

    Debug Exercises

---
+++ Lab/DebugTypeCoercionErrorsInABuggyApp.js
@@ -0,0 +1,26 @@
+// User Stories:
+
+// You should have a variable named firstResult that correctly adds the numbers 5 and 10 to produce the value 15.
+// You should have a variable named secondResult that correctly subtracts 5 from 8 to produce the value 3.
+// You should have a variable named thirdResult that produces the value 6 by adding two numbers. Replace the boolean currently used in the expression with a number.
+// You should have a variable named fourthResult that produces the value 8 by adding two numbers. Replace the boolean currently used in the expression with a number.
+// You should have a variable named fifthResult that correctly multiplies two numbers to get the product 20. Replace the string currently used in the expression with a number.
+// You should have a variable named sixthResult that correctly adds two numbers to produce the value 22. Replace the null currently used in the expression with a number.
+
+const firstResult = 5 + 10;
+console.log(`5 + 10 = ${firstResult}`);
+
+const secondResult = 8 - 5;
+console.log(`8 - 5 = ${secondResult}`);
+
+const thirdResult = 1 + 5;
+console.log(`1 + 5 = ${thirdResult}`);
+
+const fourthResult = 0 + 8;
+console.log(`0 + 8 = ${fourthResult}`);
+
+const fifthResult = 10 * 2;
+console.log(`10 * 2 = ${fifthResult}`);
+
+const sixthResult = 0 + 22;
+console.log(`0 + 22 = ${sixthResult}`);
