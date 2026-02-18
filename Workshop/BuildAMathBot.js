1c8666bdc3998a01f1cfc19192da8e02ebaf5fa9
lucysoraire <luciasoraire@gmail.com>
2026-02-18 19:42:13 -0300

    Exercise 4

---
+++ Workshop/BuildAMathBot.js
@@ -0,0 +1,28 @@
+// For this first step, start by creating a variable called botName and assign it the string value of "MathBot".
+// Then, create another variable called greeting and assign it the sentence "Hi there! My name is [botName goes here] and I am here to teach you about the Math object!". In place of the [botName goes here] placeholder, use the botName variable.
+// You are free to use template literals or string concatenation with the + operator to achieve this.
+// Finally, log the greeting variable to the console.
+const botName = "MathBot";
+const greeting = `Hi there! My name is ${botName} and I am here to teach you about the Math object!`;
+console.log(greeting);
+
+// The next part of the workshop will review how the Math.random() method works.
+// Start by adding another console.log() that logs the string "The Math.random() method returns a pseudo random number greater than or equal to 0 and less than 1." to the console.
+console.log("The Math.random() method returns a pseudo random number greater than or equal to 0 and less than 1.");
+
+// Now, it is time to generate a random number using the Math.random() method.
+// Create a variable called randomNum and assign it the value of the result of calling the Math.random() method.
+// Then, log the randomNum variable to the console.
+// Try adding a space in the code to re-run the bot and see different random numbers that are generated.
+let randomNum = Math.random();
+console.log(randomNum);
+
+// Start by adding another console.log() that logs the message "Now, generate a random number between two values." to the console.
+// Then, create a variable called min and assign it the value of 1 and create a variable called max and assign it the value of 100.
+// In the next step, you will generate a random number between these two values.
+console.log("Now, generate a random number between two values.");
+const min = 1;
+const max = 100;
+
+// Create a variable called randomNum2 and assign it the result of generating a value between the min and max values.
+// Then, log the randomNum2 variable to see the result. Try refreshing the page to see different results.
